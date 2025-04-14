985f096c03c583cc1130a1a14dad16babed7adbd
zydniyas <94290366+zydniyas@users.noreply.github.com>
2025-04-14 22:03:30 +0530

    kanban done

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Header } from "../components";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { kanbanData, kanbanGrid } from "../data/dummy";
 
 function Kanban() {
   return (
-    <div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, i) => (
+            <ColumnDirective key={i} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   );
 }
